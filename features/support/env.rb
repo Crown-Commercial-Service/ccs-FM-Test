580fec7472df6364217c62d7c5745fb834405b9e
Toyinccs <oluwatoyin.otaiku@crowncommercial.gov.uk>
2020-11-09 14:49:07 +0000

    Test data

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,22 +18,18 @@ require_relative '../support/login_helper'
 require_relative '../support/capybara_driver.helper'
 
 World(Pages)
-World(WaitUntil)
 
 configure = YAML.load_file("config/environment.yml")
 ENV['TEST_ENV'] ||= 'cmpdev'
 test_env = ENV['TEST_ENV'].downcase
 ENV['DRIVER'] ||=  :selenium_chrome
-ENV['MAX_WAIT_TIME'] ||= '30'
+ENV['MAX_WAIT_TIME'] ||= '5'
 ENV['ACCOUNT_USERNAME'] ||= configure[test_env]['account_username']
 ENV['ACCOUNT_PASSWORD'] ||= configure[test_env]['account_password']
+ENV['BUYER_USERNAME'] ||= configure[test_env]['buyer_username']
+ENV['BUYER_PASSWORD'] ||= configure[test_env]['buyer_password']
 ENV['HOST'] ||= configure[test_env]['host']
 
 MAX_OPACITY_WAIT_TIME ||= 120
-TEST_DATA ||= CSV.parse(File.read('rates.csv'), headers: true)
-FM_RATES ||= JSON.parse(File.read('fm-calculator-rates.json'), symbolize_names: false)
-DEV_TEST ||= Roo::Spreadsheet.open('RM3830 Direct Award Data (for Dev & Test).xlsx')
-DEV_TEST ||= Roo::Excelx.new('RM3830 Direct Award Data (for Dev & Test).xlsx')
-BUYER_DETAIL ||= YAML.load_file('buyer_detail.yml')
 require File.dirname(__FILE__) + '/../../features/support/capybara_driver.helper'
 require File.dirname(__FILE__) + '/../../features/support/login_helper'
